25b3fac2b8d003b81a3ecba5ad1d382897528852
theantidoak <tcdoak@gmail.com>
2022-03-30 08:48:35 -0400

    Redo Game

--- main.js
+++ main.js
@@ -1,104 +1,40 @@
+const buttons = document.querySelectorAll('.button');
+const newGame = document.querySelector('.game');
+let playerScore = 0;
+let computerScore = 0;
 
-let para = document.querySelector('.pp');
-let para2 = document.querySelector('.pc');
-let para3 = document.querySelector('.pd');
-let gameButton = document.querySelector('.game');
-gameButton.addEventListener('click', playGame);
+buttons.forEach((button) => button.addEventListener('click', buttonClick));
+newGame.addEventListener('click', restart);
+
+function buttonClick() {
+  const computerAnswer = randomizeCompAnswer();
+  const userAnswer = this.value;
+
+  if (computerAnswer == userAnswer) {
+    console.log('its a tie ' + computerAnswer + ' ' + userAnswer);
+  } else if (computerAnswer == 'rock' && userAnswer == 'paper' 
+    || computerAnswer == 'paper' && userAnswer == 'scissors'
+    || computerAnswer == 'scissors' && userAnswer == 'rock') {
+      console.log('Player Wins!');
+      console.log('Player Score = ' + ++playerScore);
+  } else {
+      console.log('Computer Wins!');
+      console.log('Computer Score = ' + ++computerScore);
+  }
+  if (playerScore == 5 || computerScore == 5) {
+    console.log('Game Over');
+    playerScore = 0;
+    computerScore = 0;
+    buttons.forEach((button) => button.removeEventListener('click', buttonClick));
+  }
+}
+
+function restart() {
+  buttons.forEach((button) => button.addEventListener('click', buttonClick));
+}
 
 /* This function generates a random answer from the opponent side. */
 function randomizeCompAnswer() {
   let thing = ['rock', 'paper', 'scissors']
   return thing[Math.floor(Math.random()*thing.length)] 
-}
-
-/* This function prompts an answer from the user, compares the answer to the opponent answer, 
-and gives a result and response.*/
-function playRound(playerSelection, computerSelection=randomizeCompAnswer()) {
-
-  playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
-  let element = document.getElementById("newLine");
-
-  if (playerSelection === 'rock' && computerSelection === 'paper' ||
-  playerSelection === 'scissors' && computerSelection === 'rock' ||
-  playerSelection === 'paper' && computerSelection === 'scissors') {
-    let result =
-      playerSelection === 'rock' && computerSelection === 'paper' ?
-      "You Lose! Paper beats Rock... <br>" :
-      playerSelection === 'paper' && computerSelection === 'scissors' ?
-      "You Lose! Scissors beats Paper... <br>" :
-      playerSelection === 'scissors' && computerSelection === 'rock' ?
-      "You Lose! Rock beats Scissors... <br>" :
-      null;
-    element.innerHTML += result;
-    updateScore(result);
-  } else if (playerSelection === 'paper' && computerSelection === 'rock' ||
-  playerSelection === 'rock' && computerSelection === 'scissors' ||
-  playerSelection === 'scissors' && computerSelection === 'paper') {
-    let result =
-      playerSelection === 'paper' && computerSelection === 'rock' ?
-      "You Win! Paper beats Rock... <br>" :
-      playerSelection === 'rock' && computerSelection === 'scissors' ?
-      "You Win! Rock beats Scissors... <br>" :
-      playerSelection === 'scissors' && computerSelection === 'paper' ?
-      "You Win! Scissors beats Paper... <br>" :
-      null;
-    element.innerHTML += result;
-    updateScore(result);
-  } else if (playerSelection === computerSelection) {
-    let result = "It's a tie... <br>";
-    element.innerHTML += result;
-    updateScore(result);
-  } else { 
-    let result = "That's not how you play the game. Point for Computer... <br>";
-    element.innerHTML += result;
-    updateScore(result);
-  };
-}
-
-/* This function updates the score by comparing to an html element. */
-function updateScore(result) {
-  if (result.charAt(4) == 'L' || result.charAt(0) == 'T') {
-    para2.textContent = para2.textContent === '0' ? para2.textContent = '1' :
-    para2.textContent === '1' ? para2.textContent = '2' :
-    para2.textContent === '2' ? para2.textContent = '3' :
-    para2.textContent === '3' ? para2.textContent = '4' :
-    para2.textContent === '4' ? para2.textContent = '5 Woah! That\'s some bad luck...' :
-    para2.textContent = "Restart Game";
-  } else if (result.charAt(4) == 'W') {
-    para.textContent = para.textContent === '0' ? para.textContent = '1' :
-    para.textContent === '1' ? para.textContent = '2' :
-    para.textContent === '2' ? para.textContent = '3' :
-    para.textContent === '3' ? para.textContent = '4' :
-    para.textContent === '4' ? para.textContent = '5 Woah! That\'s some good luck...' :
-    para.textContent = "Restart Game";
-  } else {
-    null;
-  };
-}
-
-/* This function gives the player the final message */
-function giveFinalScore() {
-  if (parseInt(para.textContent) === parseInt(para2.textContent)) {
-    return "<br>It's a tie!"
-  } else if (parseInt(para.textContent) > parseInt(para2.textContent)) {
-    return "<br>You win the game!"
-  } else if (parseInt(para.textContent) < parseInt(para2.textContent)) {
-    return "<br>You lose. Play again!"
-  };
-}
-
-/* This function loops the playRound function five times and gives a final score. */
-function playGame() {
-  let element = document.getElementById("newLine");
-    
-  
-  para.textContent = 0;
-  para2.textContent = 0;
-  element.innerHTML = "Verdict: <br/>";
-  for (let i = 0; i < 5; i++) {
-    outcome = playRound();
-    console.log(outcome);
-  };
-  element.innerHTML += `${giveFinalScore()}`;
-  
 }

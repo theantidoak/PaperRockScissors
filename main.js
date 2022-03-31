1b51d076d699405c0dfb5c83014e7b9c6cbb8068
theantidoak <tcdoak@gmail.com>
2022-03-31 13:04:41 -0400

    Cleaning merge

--- main.js
+++ main.js
@@ -8,17 +8,8 @@ const tally = document.querySelectorAll('.L');
 const tallyR = document.querySelectorAll('.R');
 const header = document.querySelector('.instructions h1');
 
-<<<<<<< HEAD
-let para = document.querySelector('.pp');
-let para2 = document.querySelector('.pc');
-let para3 = document.querySelector('.pd');
-let gameButton = document.querySelector('.game');
-gameButton.addEventListener('click', playGame);
-let element = document.getElementById("newLine");
-=======
 buttons.forEach((button) => button.addEventListener('click', buttonClick));
 newGame.addEventListener('click', restart);
->>>>>>> rps-ui
 
 // Play Game
 function buttonClick() {
@@ -48,50 +39,6 @@ function buttonClick() {
     emojis.forEach(emoji => emoji.id == computerAnswer ? emoji.style.marginTop = '-10rem' : null);
   }, 500);
 
-<<<<<<< HEAD
-  playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
-
-  if (playerSelection === 'rock' && computerSelection === 'paper' ||
-  playerSelection === 'scissors' && computerSelection === 'rock' ||
-  playerSelection === 'paper' && computerSelection === 'scissors') {
-    let result =
-      playerSelection === 'rock' && computerSelection === 'paper' ?
-      "You Lose! Paper beats Rock... <br>" :
-      playerSelection === 'paper' && computerSelection === 'scissors' ?
-      "You Lose! Scissors beats Paper... <br>" :
-      playerSelection === 'scissors' && computerSelection === 'rock' ?
-      "You Lose! Rock beats Scissors... <br>" :
-      null;
-    element.innerHTML += result;
-    updateScore(result);
-  } else if (playerSelection === 'paper' && computerSelection === 'rock' ||
-  playerSelection === 'rock' && computerSelection === 'scissors' ||
-  playerSelection === 'scissors' && computerSelection === 'paper') {
-    let result =
-      playerSelection === 'paper' && computerSelection === 'rock' ?
-      "You Win! Paper beats Rock... <br>" :
-      playerSelection === 'rock' && computerSelection === 'scissors' ?
-      "You Win! Rock beats Scissors... <br>" :
-      playerSelection === 'scissors' && computerSelection === 'paper' ?
-      "You Win! Scissors beats Paper... <br>" :
-      null;
-    element.innerHTML += result;
-    updateScore(result);
-  } else if (playerSelection === computerSelection) {
-    let result = playerSelection === 'rock' && computerSelection === 'rock' ? 
-    "It's a ROCK tie... <br>" : playerSelection === 'paper' && computerSelection === 'paper' ?
-    "It's a PAPER tie... <br>" : playerSelection === 'scissors' && computerSelection === 'scissors' ?
-    "It's a SCISSORS tie... <br>" : null;
-    element.innerHTML += result;
-    updateScore(result);
-  } else { 
-    let result = `That's not how you play the game. 
-      '${playerSelection}' is not an answer. Point for Computer... <br>`;
-    element.innerHTML += result;
-    updateScore(result);
-  };
-}
-=======
   // End Round
   setTimeout(() => {
     // Remove Transform
@@ -102,7 +49,6 @@ function buttonClick() {
 
     // Replace event listener
     buttons.forEach((button) => button.addEventListener('click', buttonClick));
->>>>>>> rps-ui
 
     // Track Score
     if (winner) {
@@ -154,33 +100,8 @@ function restart() {
   tallyR.forEach(tallyr => tallyr.style.backgroundColor = 'rgb(36, 147, 221)');
 }
 
-<<<<<<< HEAD
-/* This function loops the playRound function five times and gives a final score. */
-function playGame() {
-  let element = document.getElementById("newLine");
-    
-  
-  para.textContent = 0;
-  para2.textContent = 0;
-  element.innerHTML = "Verdict: <br/>";
-  for (let i = 0; i < 5; i++) {
-    outcome = playRound();
-    console.log(outcome);
-  };
-  element.innerHTML += `${giveFinalScore()}`;
-  
-}
-
-/* Use element.appendChild(para3) to display each score.
-Step 1: Create the para variable with document.createElement('p')
-Step 2: Create loop with while(), but also try with for().
-Step 3: With each loop, para.textContent = " "
-Step 4: After Everything, Before the next loop, append the para to the div with element.appendChild(para)
-*/
-=======
 //Generate a random answer for the computer
 function randomizeCompAnswer() {
   let thing = ['rock', 'paper', 'scissors']
   return thing[Math.floor(Math.random()*thing.length)] 
 }
->>>>>>> rps-ui
